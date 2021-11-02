ace3d9d338fc1feb50a9efcd70395a134969b68c
AgarwalPakhi <93395767+AgarwalPakhi@users.noreply.github.com>
2021-11-02 20:47:29 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+function add() 
+{
+    event.preventDefault();
+    let a = parseFloat(document.getElementById("a").value);
+    let b = paeseFloat(document.getElementById("b").value);
+    document.getElementById("answer").value = a + b;
+    document.getElementById("ans").style.display = "block";
+    alert("hfrbvhbhb")
+}
